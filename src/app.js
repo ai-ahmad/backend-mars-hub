632ff9e758572aeaf1a588d1ed085dc8fcc9102f
ai-ahmad <ahmedovalmat8@gmail.com>
2025-04-15 18:38:21 +0500

    test

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/reels", reelsRouter);
-app.use("/api/v1/publication", publicationRouter);
+app.use("/api/v1/publications", publicationRouter);
 app.use("/api/v1/task-room", roomRoutes);
 app.use("/api/v1/comments", commentRouter);
 
